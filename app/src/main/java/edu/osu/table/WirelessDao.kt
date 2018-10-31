90d0f391c382db14fd80840691b8c305d6eb005c
Michael Kines <puXnam-2hurre-gavduq>
2018-10-31 17:07:50 -0400

    - Changed date to a "Long" for simplicity - Updated Gradle files to support Kotlin / Java stuff associated with the Database - Instantiated a Database (example) in RecommednationActivity.kt - I am able to write to database and read from it - DbWorkerThread to do async tasks of storing to and reading from database - Removed music file (ahh...) - Compiled, and runs on both emulator and physical hardware

--- app/src/main/java/edu/osu/table/WirelessDao.kt
+++ app/src/main/java/edu/osu/table/WirelessDao.kt
@@ -11,8 +11,11 @@ interface WirelessDao {
     @Query("SELECT * from wirelessData")
     fun getAll(): List<WirelessData>
 
-    @Query("SELECT * from wirelessData WHERE rss_dbm > :rss_dbm")
+    /*@Query("SELECT * from wirelessData WHERE rss_dbm > :rss_dbm")
     fun getRssGreaterThan(rss_dbm: Double): List<WirelessData>
+
+    @Query("SELECT * FROM wirelessData ORDER BY rss_dbm DESC")
+    fun getRssSort(): List<WirelessData>*/
 
     @Insert(onConflict = REPLACE)
     fun insert(wirelessData: WirelessData)
--- app/src/main/java/edu/osu/table/WirelessData.kt
+++ app/src/main/java/edu/osu/table/WirelessData.kt
@@ -8,19 +8,18 @@ import java.util.*
 @Entity(tableName = "wirelessData")
 data class WirelessData(@PrimaryKey(autoGenerate = true) var id: Long?,
                        @ColumnInfo(name = "date") var CurDate: Long,
-                       @ColumnInfo(name = "ssid") var SSID: String,
+                       //@ColumnInfo(name = "ssid") var SSID: String,
                        @ColumnInfo(name = "mac_address") var MAC_Address: String,
-                       @ColumnInfo(name = "security") var Security: String,
-                       @ColumnInfo(name = "rss_dbm") var RSSdBm: Int,
-                       @ColumnInfo(name = "chan_freq") var ChanFreq: Double,
-                       @ColumnInfo(name = "thoughput_Mbps") var ThroughputMpbs: Double,
-                       @ColumnInfo(name = "channel_bw") var ChBW: Double,
+                       //@ColumnInfo(name = "security") var Security: String,
+                       //@ColumnInfo(name = "rss_dbm") var RSSdBm: Int,
+                       //@ColumnInfo(name = "chan_freq") var ChanFreq: Double,
+                       //@ColumnInfo(name = "thoughput_Mbps") var ThroughputMpbs: Double,
+                       //@ColumnInfo(name = "channel_bw") var ChBW: Double,
                        @ColumnInfo(name = "battery_percentage") var BatteryPerc: Double
 
 
 
 ){
-    constructor():this(null,0,  "",   "",   "",
-            -1000,   0.0,   -1.0, 0.0, -1.0)
+    constructor():this(null,0,  "", -1.0)
 
 }
